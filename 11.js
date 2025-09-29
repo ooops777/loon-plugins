94bed7a0267ca37bc7eeaf494e271378b9dbced8
ooops777 <121816359+ooops777@users.noreply.github.com>
2025-09-30 08:26:34 +0900

    Update 11.js

--- 11.js
+++ 11.js
@@ -1,1 +1,14 @@
-$done({body: JSON.stringify({environment:"Production",receipt:{in_app:[{quantity:"1",product_id:"com.ft.ftchinese.mobile.subscription.vip",expires_date:"2099-04-02 22:22:22 Etc/GMT",expires_date_ms:"3911571742000",expires_date_pst:"2099-04-02 22:22:22 America/Los_Angeles",in_app_ownership_type:"PURCHASED"}]},latest_receipt_info:[{quantity:"1",product_id:"com.ft.ftchinese.mobile.subscription.vip",expires_date:"2099-04-02 22:22:22 Etc/GMT",expires_date_ms:"3911571742000",expires_date_pst:"2099-04-02 22:22:22 America/Los_Angeles",in_app_ownership_type:"PURCHASED"}],latest_receipt:"NA",pending_renewal_info:[{auto_renew_product_id:"com.ft.ftchinese.mobile.subscription.vip",product_id:"com.ft.ftchinese.mobile.subscription.vip",auto_renew_status:"0"}],status:0})});
+var baby = JSON.parse($response.body);
+
+baby = {
+  "paywall": 0,
+  "premium": 1,
+  "expire": "4092599349",
+  "standard": 1,
+  "v": 2099,
+  "campaign_code": "",
+  "latest_duration": "yearly",
+  "addon": 0
+};
+
+$done({ body: JSON.stringify(baby) });
